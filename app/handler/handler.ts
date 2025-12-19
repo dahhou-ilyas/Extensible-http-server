3c215ca7a41537450d6fe039183067e3e4ea8337
Ilyas Dahhou <ilyas.dahhou@adria-bt.com>
2025-12-19 17:00:44 +0100

    handle file

--- app/handler/handler.ts
+++ app/handler/handler.ts
@@ -1,6 +1,7 @@
 import type { HttpRequest, HttpResponse } from "../type/type";
 import fs from "fs";
-import { readFile } from "fs/promises";
+import { readFile,mkdir,writeFile } from "fs/promises";
+import { join } from "path";
 
 
 export function simpleSuccesForGet(req: HttpRequest, res: HttpResponse){
@@ -41,6 +42,25 @@ export async function fileHandler(req: HttpRequest, res: HttpResponse){
             res.statusCode=404
             res.headers["Content-Type"]="application/octet-stream"
         }
+    }catch{
+        console.error("error while reading file")
+    }
+    
+}
+
+
+export async function saveFile(req: HttpRequest, res: HttpResponse){
+
+    const fileName = req?.params?.filename;
+
+    const body = req.Body as string;
+    const baseDir = "/tmp/data/codecrafters.io/http-server-tester";
+
+
+    const filePath = join(baseDir, fileName);
+    try{
+        await mkdir(baseDir, { recursive: true });
+        await writeFile(filePath, body, "utf8");
     }catch{
         console.error("error while reading file")
     }
